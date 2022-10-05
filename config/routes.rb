3387815de986455bdf0b7f722d1a83b3783e6255
GeorgePires <georgeasp.pi@gmail.com>
2022-10-04 22:29:34 -0300

    :ok_hand: style/ prettier for ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :contacts
-  root to: "static_pages#index"
+  root to: 'static_pages#index'
 
   get 'about', to: 'static_pages#about'
   get 'contac', to: 'static_pages#contac'
