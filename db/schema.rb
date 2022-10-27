7f5fc9a5708a3961c0aed9ddf3df0523e66b362d
GeorgePires <georgeasp.pi@gmail.com>
2022-10-27 01:10:27 -0300

    :heavy_plus_sign: build/ migration avatar to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_05_001241) do
+ActiveRecord::Schema.define(version: 2022_10_26_200459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_10_05_001241) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "avatar"
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
