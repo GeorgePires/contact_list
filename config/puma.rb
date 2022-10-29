44f4c0e56f7a6ddd01563b861495abaa0fc8866a
GeorgePires <georgeasp.pi@gmail.com>
2022-10-29 01:02:32 -0300

    :rocket: test/ Deploy - not working

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,6 @@
+require "fileutils"
+FileUtils.mkdir_p("tmp/pids")
+
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
